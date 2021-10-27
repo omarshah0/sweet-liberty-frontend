e1206bc71bc304c63d2f12627ae344443512df6d
Omar Shah <60596111+omarshah0@users.noreply.github.com>
2021-10-27 16:17:03 +0500

    Recoil Persist SSR Fix

--- src/system/Atom/cartState.js
+++ src/system/Atom/cartState.js
@@ -1,6 +1,9 @@
 import { atom } from "recoil"
 import { recoilPersist } from "recoil-persist"
 
+const localStorage =
+  typeof window !== `undefined` ? require("node-localstorage") : null
+
 const { persistAtom } = recoilPersist({
   key: "recoil-seet-liberty-shopping-cart", // this key is using to store data in local storage
   storage: localStorage, // configurate which stroage will be used to store the data
