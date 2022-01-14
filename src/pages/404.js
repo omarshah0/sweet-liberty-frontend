16acd0c3c4d3bd2074bfd7262b7f280aa60eb917
Omar Shah <60596111+omarshah0@users.noreply.github.com>
2022-01-14 10:51:57 +0500

    Lazy Testing

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import React from "react"
 
-const NotFoundPage = () => <h1>Cheema</h1>
+const NotFoundPage = () => <h1>Page Not Found</h1>
 
 export default NotFoundPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,33 +5,74 @@ import loadable from "@loadable/component"
 import Layout from "../components/Layout"
 import Seo from "../components/SEO"
 import Hero from "../components/Hero"
-import PursuingExcellence from "../components/PursuingExcellence"
-import MenuBanner from "../components/MenuBanner"
-import MerchSection from "../components/MerchSection"
-import BookingSection from "../components/BookingSection"
-import NewsEvents from "../components/NewEvents"
-import DoGoodWork from "../components/DoGoodWork"
-import ShowAndEvents from "../components/ShowAndEvents"
 
 import ScrollTop from "../components/ScrollTop"
 import { Main } from "../components/UI"
+
+const PursuingExcellence = loadable(
+  () => import("../components/PursuingExcellence"),
+  {
+    fallback: (
+      <p className="font-bebas text-[60px] leading-[60px] text-brandLight md:text-[80px] md:leading-[65px] lg:text-[116px] lg:leading-[119px]">
+        This is sweet liberty. Enjoyed by tourists, loved by locals.
+      </p>
+    ),
+  }
+)
 
 const FeaturedSection = loadable(
   () => import("../components/FeaturedSection"),
   {
     fallback: (
       <div>
-        <p>The Miami Martini</p>
-        <p>The Pina Colada</p>
+        <p>The Florida Cocktail</p>
+        <p>Our Pina Colada</p>
+        <p>Blue Zombie Cocktail</p>
       </div>
     ),
   }
 )
+
+const MenuBanner = loadable(() => import("../components/MenuBanner"), {
+  fallback: (
+    <p>PursuingExcellence Sprited Awards Logo Best Bars Logo Timeout Logo</p>
+  ),
+})
+
+const MerchSection = loadable(() => import("../components/MerchSection"), {
+  fallback: <p>Order Your Sweet LibertySweet Liberty Merch NowMerch Now</p>,
+})
+const MiamiIsShit = loadable(() => import("../components/MiamiIsShit"), {
+  fallback: <p>Miami Is The Shit Bro</p>,
+})
+
 const StylishPursue = loadable(() => import("../components/StylishPursue"), {
   fallback: <p>Pursing Happiness</p>,
 })
-const MiamiIsShit = loadable(() => import("../components/MiamiIsShit"), {
-  fallback: <p>Miami Is Shit</p>,
+
+const BookingSection = loadable(() => import("../components/BookingSection"), {
+  fallback: (
+    <div>
+      <p>Book A Party</p>,<p>Make A Reservation</p>,
+    </div>
+  ),
+})
+
+const NewsEvents = loadable(() => import("../components/NewEvents"), {
+  fallback: <p>News and Events</p>,
+})
+
+const DoGoodWork = loadable(() => import("../components/DoGoodWork"), {
+  fallback: <p>Our greatest award is being able to open every day</p>,
+})
+
+const ShowAndEvents = loadable(() => import("../components/ShowAndEvents"), {
+  fallback: (
+    <p>
+      Fruit cock tail drag show <br />
+      Every sunday 12:30 PM & 2:30 PM shows
+    </p>
+  ),
 })
 
 const TripleSeatModal = loadable(() =>
