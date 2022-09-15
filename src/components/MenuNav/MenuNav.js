cfe77a0347409b38d7fecb052a58be9078be3ca8
OmarShah <oemyoem55@gmail.com>
2022-09-15 11:21:22 +0500

    Added PDF and Fixed Margin Issue

--- src/components/MenuNav/MenuNav.js
+++ src/components/MenuNav/MenuNav.js
@@ -24,15 +24,25 @@ const MenuNav = ({ slug }) => {
         className={`flex justify-between mb-24 pb-4 overflow-x-auto nav__${slug} nav-scroll`}
       >
         {allDatoCmsMenu.nodes.map((m, i) => (
-          <li key={m.slug}>
+          <li
+            key={m.slug}
+            className={i === 0 ? "ml-0 md:ml-0" : "ml-4 md:ml-6"}
+          >
             <MenuNavBtn
               title={m.menuNameNavigation}
               icon={m.menuIcon.url}
               slug={m.slug}
-              className={i === 0 ? "ml-0 md:ml-0" : "ml-4 md:ml-6"}
             />
           </li>
         ))}
+        <li className="ml-4 md:ml-6">
+          <MenuNavBtn
+            title="Spirits List"
+            icon={null}
+            slug="https://drive.google.com/file/d/1xQg7bkMT7KLRZyVNc_FU89QU-m5bLiP0/view"
+            outerLink
+          />
+        </li>
       </ul>
     </nav>
   )
--- src/components/UI/MenuNavBtn.js
+++ src/components/UI/MenuNavBtn.js
@@ -1,7 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const MenuNavBtn = ({ icon, title, slug, className }) => {
+const MenuNavBtn = ({ icon, title, slug, className, outerLink }) => {
+  if (outerLink) {
+    return (
+      <a
+        href={slug}
+        target="__blank"
+        rel="noreferrer"
+        className={`flex min-w-max font-sourceSansProBold px-8 py-4 rounded transition tab__${slug} ${
+          className || ""
+        }`}
+      >
+        {icon && <img src={icon} alt={title} className="mr-3" />}
+        <span className="block">{title}</span>
+      </a>
+    )
+  }
+
   return (
     <Link
       to={`/menu/${slug}`}
@@ -9,7 +25,7 @@ const MenuNavBtn = ({ icon, title, slug, className }) => {
         className || ""
       }`}
     >
-      <img src={icon} alt={title} className="mr-3" />
+      {icon && <img src={icon} alt={title} className="mr-3" />}
       <span className="block">{title}</span>
     </Link>
   )
